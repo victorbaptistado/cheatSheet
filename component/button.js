a02db22fda7ba58da004615772323df35f8b7785
victorbaptistado <victorbaptistado@gmail.com>
2022-11-30 23:09:13 +0000

    Create button component

---
+++ component/button.js
@@ -0,0 +1,10 @@
+
+function showCode(displayCode, showCode){
+    document.getElementById(displayCode).style.display = "block";
+    document.getElementById(showCode).style.display = "none";
+}
+
+function closeCode(displayCode, showCode){
+    document.getElementById(showCode).style.display = "block";
+    document.getElementById(displayCode).style.display = "none";
+}
