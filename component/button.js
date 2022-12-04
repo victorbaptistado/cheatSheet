3ede86f93b8933ddeb16763ea307c6c113d392c9
victorbaptistado <victorbaptistado@gmail.com>
2022-12-04 15:26:36 +0000

    API, REST, Json added, CSS modified

--- component/button.js
+++ component/button.js
@@ -8,10 +8,3 @@ function closeCode(displayCode, showCode){
     document.getElementById(showCode).style.display = "block";
     document.getElementById(displayCode).style.display = "none";
 }
-
-function exe(){
-    console.log("I’m learning about");
-    setTimeout(() => { console.log("Event Loop");}, 2000);
-    console.log("the");
-}
-exe();
